64e79eeb24d93d6f235f7c99c58c7d5a165cd8d2
hazadus <hazadus7@gmail.com>
2022-10-02 01:24:08 +0300

    Lesson #3 done.

--- events/views.py
+++ events/views.py
@@ -1,15 +1,17 @@
+from datetime import datetime
+
 from django.shortcuts import render
 import calendar
 from calendar import HTMLCalendar
 
 
-def home(request, year=2022, month='October'):
+def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
     month = month.title()
     month_number = list(calendar.month_name).index(month)
 
     clndr = HTMLCalendar().formatmonth(year, month_number)
 
-    return render(request, 'home.html', {
+    return render(request, 'events/home.html', {
         'year': year,
         'month': month,
         'month_number': month_number,
