940538c0f33d092daaa29999e29a109bb9d6724f
hazadus <hazadus7@gmail.com>
2022-10-02 00:48:44 +0300

    Lesson #2 done.

--- events/urls.py
+++ events/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('<int:year>/<str:month>/', views.home, name='home'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
 
 
-def home(request):
-    return render(request, 'home.html', {})
+def home(request, year=2022, month='October'):
+    month = month.title()
+    month_number = list(calendar.month_name).index(month)
+
+    clndr = HTMLCalendar().formatmonth(year, month_number)
+
+    return render(request, 'home.html', {
+        'year': year,
+        'month': month,
+        'month_number': month_number,
+        'clndr': clndr
+    })
