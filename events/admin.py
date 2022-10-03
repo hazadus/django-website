66f6aa25bcc39e7cadb4764d3d2b53afa0acccaf
hazadus <hazadus7@gmail.com>
2022-10-03 15:44:21 +0300

    DB model for venues extended, tags added.

--- events/admin.py
+++ events/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models import Venue
 from .models import User
 from .models import Event
+from .models import Tag
 
 admin.site.register(Venue)
 admin.site.register(User)
 admin.site.register(Event)
+admin.site.register(Tag)
--- events/models.py
+++ events/models.py
@@ -1,13 +1,27 @@
 from django.db import models
 
 
+class Tag(models.Model):
+    name = models.CharField('Tag Name', max_length=32)
+
+    def __str__(self):
+        return f'#{self.name.lower()}'
+
+
 class Venue(models.Model):
     name = models.CharField('Venue Name', max_length=128)
     address = models.CharField('Venue Address', max_length=256)
-    zip_code = models.CharField('Zip Code', max_length=8)
-    phone = models.CharField('Contact Phone', max_length=16)
-    website = models.URLField('Website URL')
-    email = models.EmailField('Contact Email')
+    city = models.CharField('Location City', max_length=32, blank=True)
+    zip_code = models.CharField('Zip Code', max_length=8, blank=True)
+    phone = models.CharField('Contact Phone', max_length=16, blank=True)
+    description = models.TextField('Description', blank=True)
+    how_to_get_there = models.TextField('How to get there (transport, etc.)', blank=True)
+    website = models.URLField('Website URL', blank=True)
+    banner_pic = models.CharField('Banner Picture Filename', max_length=64, blank=True)
+    email = models.EmailField('Contact Email', blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+    yandex_map_widget = models.TextField('Yandex Map Widget', blank=True)
+    yandex_reviews_widget = models.TextField('Yandex Reviews Widget', blank=True)
 
     def __str__(self):
         return self.name
@@ -25,10 +39,10 @@ class User(models.Model):
 class Event(models.Model):
     name = models.CharField('Event Name', max_length=128)
     event_date = models.DateTimeField('Event Date')
-    venue = models.ForeignKey(Venue, blank=True, null=True, on_delete=models.CASCADE)
+    venue = models.ForeignKey(Venue, blank=True, null=True, on_delete=models.DO_NOTHING)
     manager = models.CharField("Who's In Charge", max_length=64)
     description = models.TextField('Event Description', blank=True)
-    attendees = models.ManyToManyField(User, blank=True )
+    attendees = models.ManyToManyField(User, blank=True)
 
     def __str__(self):
         return self.name
