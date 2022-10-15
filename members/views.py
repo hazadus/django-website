297828776121bba6c6cbf2c915a1fe4da7b3ab49
hazadus <hazadus7@gmail.com>
2022-10-15 16:45:38 +0300

    Lesson #21 done – Login page works.

--- members/views.py
+++ members/views.py
@@ -4,4 +4,16 @@ from django.contrib import messages
 
 
 def login_user(request):
-    return render(request, 'authenticate/login.html', {})
+    # https://docs.djangoproject.com/en/4.1/topics/auth/default/#how-to-log-a-user-in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Failed to log in.')
+            return redirect('login_user')
+    else:
+        return render(request, 'authenticate/login.html', {})
