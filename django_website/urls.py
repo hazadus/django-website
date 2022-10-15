986ce5435f399d698a7d458f3c1d7e644c217691
hazadus <hazadus7@gmail.com>
2022-10-15 15:57:51 +0300

    Lesson #21 - sync...

--- django_website/urls.py
+++ django_website/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls')),
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls')),
 ]
 
 # Configure Admin panel titles
