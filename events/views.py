a1720ee6484a60a918a5966617795a918a711418
hazadus <hazadus7@gmail.com>
2022-10-21 22:52:47 +0300

    Lesson #39 done – change image on existing venue.

--- events/views.py
+++ events/views.py
@@ -165,7 +165,7 @@ def show_venue(request, venue_id):
 
 def edit_venue(request, venue_id):
     venue = Venue.objects.get(pk=venue_id)
-    form = VenueForm(request.POST or None, instance=venue)
+    form = VenueForm(request.POST or None, request.FILES or None, instance=venue)
     if form.is_valid():
         form.save()
         return redirect('show_venue', venue.id)
